cb5b6feddad2ba2656647535ee2177ff222e647c
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-09-27 17:36:41 +0530

    Create min_in_range.cpp

---
+++ Segment trees/min_in_range.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <iomanip> //for setprecision()
+#include <cstring> //for memset()
+using namespace std;
+#define ll long long
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define w(x) int x; cin>>x; while(x--)
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+void buildTree(int *a, int s, int e, int *tree, int idx)
+{
+	if (s == e)
+	{
+		tree[idx] = a[s];
+		return;
+	}
+
+	int mid = (s + e) / 2;
+	buildTree(a, s, mid, tree, 2 * idx);
+	buildTree(a, mid + 1, e, tree, 2 * idx + 1);
+	tree[idx] = min(tree[2 * idx], tree[2 * idx + 1]);
+	return;
+}
+
+int query(int *tree, int s, int e, int qs, int qe, int idx)
+{
+	//Complete overlap
+	if (s >= qs && e <= qe)
+		return tree[idx];
+
+	//No overlap
+	if (s > qe || e < qs)
+		return INT_MAX;
+
+	//Partial overlap
+	int mid = (s + e) / 2;
+	int left = query(tree, s, mid, qs, qe, 2 * idx);
+	int right = query(tree, mid + 1, e, qs, qe, 2 * idx + 1);
+	return min(left, right);
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int a[] = {1, 3, 2, -5, 6, 4};
+	int n = sizeof(a) / sizeof(int);
+
+	int *tree = new int[4 * n + 1];
+	buildTree(a, 0, n - 1, tree, 1);
+
+	w(q)
+	{
+		int l, r;
+		cin >> l >> r;
+		cout << query(tree, 0, n - 1, l, r, 1) << endl;
+	}
+}
