5076ac36702a5d3240227e9444c5acb3b1f0558c
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-10-06 04:13:56 +0530

    Update fibonacci series (bottom up).cpp

--- Dynamic programming/fibonacci series (bottom up).cpp
+++ Dynamic programming/fibonacci series (bottom up).cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define w(x) int x; cin>>x; while(x--)
 #define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-int fib(int n)
+int fib(int n)  //Space complexity: O(N)
 {
 	int dp[101] = {};
 	dp[1] = 1;
@@ -23,6 +23,20 @@ int fib(int n)
 	return dp[n];
 }
 
+int optimisedSpace_fib(int n)  //Space complexity: O(1)
+{
+	if(n==0 || n==1)
+		return n;
+	
+	int a=0,b=1,c;
+	for(int i=2;i<=n;i++)
+	{
+		c=a+b;
+		a=b,b=c;
+	}
+	return c;
+}
+	
 int main()
 {
 	/*#ifndef ONLINE_JUDGE
@@ -33,4 +47,5 @@ int main()
 	int n = 5;
 
 	cout << fib(n);
+	cout << optimisedSpace_fib(n);
 }
