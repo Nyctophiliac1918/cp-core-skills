48f34ff45a17218ed00e7161ced3ec99132f4929
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-10-17 00:49:14 +0530

    Update and rename max sum subarray.cpp to max sum subarray (kadane's algo) .cpp

--- Dynamic programming/max sum subarray (kadane's algo) .cpp
+++ Dynamic programming/max sum subarray (kadane's algo) .cpp
@@ -29,6 +29,26 @@ int maxSum(int a[], int n)
 	return max_so_far;
 }
 
+// Space optimised code
+// Kadane's algo
+int maxSumSpaceOpt(int a[], int n)
+{
+	int current_sum = 0;
+	int max_so_far = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		current_sum += a[i];
+		if (current_sum < 0)
+			current_sum = 0;
+		max_so_far = max(current_sum, max_so_far);
+	}
+
+	return max_so_far;
+}
+
+// There could be a case when all the numbers in the array are negative. You might have to print the smallest -ve number if the question says the subarray can't be empty.
+
 int main()
 {
 	/*#ifndef ONLINE_JUDGE
@@ -39,7 +59,8 @@ int main()
 	int a[] = { -3, 2, 5, -1, 6, 3, -2, 7, -5, 2};
 	int n = sizeof(a) / sizeof(int);
 
-	cout << maxSum(a, n);
+	cout << maxSum(a, n) << endl;
+	cout << maxSumSpaceOpt(a, n);
 
 	return 0;
 }
