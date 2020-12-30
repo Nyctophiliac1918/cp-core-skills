b823e063d52384c83584dd846f74cad17a8d3c45
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-12-30 20:57:37 +0530

    Create snake and ladders using BFS-SSSP.cpp

---
+++ Graphs/snake and ladders using BFS-SSSP.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<map>
+#include<queue>
+#include<list>
+#include<climits>
+using namespace std;
+
+template<typename T>
+class Graph {
+	map<T, list<T>>l;
+
+public:
+	void addEdge(int x, int y)
+	{
+		l[x].push_back(y);
+	}
+
+	int shortest_path_bfs(T src, T dest)
+	{
+		map<T, int>dist;
+		map<T, T> parent;
+		queue<T>q;
+
+		for (auto node_pair : l)
+		{
+			T node = node_pair.first;
+			dist[node] = INT_MAX;
+		}
+
+		q.push(src);
+		dist[src] = 0;
+		parent[src] = src;
+
+		while (!q.empty())
+		{
+			T node = q.front();
+			q.pop();
+			//cout << node << " ";
+
+			for (int nbr : l[node])
+			{
+				if (dist[nbr] == INT_MAX)
+				{
+					q.push(nbr);
+					//marking nbr as visited.
+					dist[nbr] = dist[node] + 1;
+					parent[nbr] = node;
+				}
+			}
+		}
+
+		T temp = dest;
+		while (temp != src)
+		{
+			cout << temp << " <-- ";
+			temp = parent[temp];
+		}
+		cout << src << endl;
+
+		// Print the src to dest way.
+
+
+		return dist[dest];
+	}
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int board[40] = {};
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[25] = 10;
+	board[17] = -13;
+	board[24] = -8;
+	board[20] = -14;
+	board[32] = -2;
+	board[34] = -22;
+
+
+	Graph<int> g;
+	for (int i = 0; i < 36; i++)
+	{
+		for (int dice = 1; dice <= 6; dice++)
+		{
+			int j = i + dice;
+			j += board[j];
+
+			if (j <= 36)
+				g.addEdge(i, j);
+		}
+	}
+	g.addEdge(36, 36);
+
+	cout << g.shortest_path_bfs(0, 36);
+}
