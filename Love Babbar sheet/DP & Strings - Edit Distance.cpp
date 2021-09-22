5344452654413e51e7d23b69a01fbafb22eb9a5e
Karan Saraswat <64465581+Nyctophiliac1918@users.noreply.github.com>
2021-09-22 14:55:32 +0530

    Create DP & Strings - Edit Distance.cpp

---
+++ Love Babbar sheet/DP & Strings - Edit Distance.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string s, string t) {
+        // Code here
+        int n = s.size(), m = t.size();
+        int dp[n+1][m+1] = {};
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            dp[0][i] = i;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]});
+            }
+        }
+    
+        return dp[n][m];
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
