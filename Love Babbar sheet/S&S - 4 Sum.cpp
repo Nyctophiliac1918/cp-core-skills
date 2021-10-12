e6936f20284a5f34123fb81f9971655222903cb0
Karan Saraswat <64465581+Nyctophiliac1918@users.noreply.github.com>
2021-10-12 13:22:41 +0530

    Update S&S - 4 Sum.cpp

--- Love Babbar sheet/S&S - 4 Sum.cpp
+++ Love Babbar sheet/S&S - 4 Sum.cpp
@@ -1,3 +1,4 @@
+// Using set
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& arr, int k) {
@@ -30,6 +31,43 @@ public:
             v.push_back({k[0], k[1], k[2], k[3]});
             it++;
         }
+        return v;
+    }
+};
+
+//without extra space just a vector to store
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& arr, int k) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<vector<int>>v;
+        for(int i=0;i<n-3;) {
+            for(int j=i+1;j<n-2;) {
+                int sum = k - (arr[i]+arr[j]);
+                int l = j+1, r=n-1;
+                // cout<<i<<" "<<j<<endl;
+                while(l<r) {
+                    // cout<<l<<" "<<r<<" "<<sum<<" "<<arr[l]+arr[r]<<endl;
+                    if(sum==arr[l]+arr[r])
+                    {
+                        v.push_back({arr[i],arr[j],arr[l],arr[r]});
+                        int t = arr[l], fr = arr[r];
+                        while(l<r && arr[l]==t)
+                            l++;
+                        while(l<r && arr[r]==fr) r--;
+                    }
+                    else if(sum<arr[l]+arr[r])
+                        r--;
+                    else l++;
+                }
+                int s = arr[j];
+                while(j<n-2 && arr[j]==s) j++;
+            }
+            int f = arr[i];
+            while(i<n-3 && arr[i]==f) i++;
+        }
+
         return v;
     }
 };
